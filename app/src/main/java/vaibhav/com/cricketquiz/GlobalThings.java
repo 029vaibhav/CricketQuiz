8c7dc26eb0e01faf602d2de478c7389bf8db342b
mobile1-vaibhav <vaibs4007@rediff.com>
2015-08-12 18:40:22 +0530

    Initial commit

---
+++ app/src/main/java/vaibhav/com/cricketquiz/GlobalThings.java
@@ -0,0 +1,221 @@
+package vaibhav.com.cricketquiz;
+
+import android.app.Application;
+
+public class GlobalThings extends Application {
+
+    public static boolean music = true;
+
+
+//    static String[] TypeEasy = {"t", "t", "t", "t", "t", "t", "t", "t", "t",
+//            "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t",
+//            "t", "t", "t"};
+//
+//	static String[] ImageEasy = {
+//			"",
+//			"",
+//			"",
+//			"",
+//			"",
+//			"",
+//			"",
+//			"",
+//			"",
+//			"",
+//			"",
+//			"",
+//			"",
+//			"",
+//			"",
+//			"",
+//			"",
+//			"",
+//			"", "", "", "", "", "", "" };
+
+    static String[] TextEasy = {
+            "Which player has maximum number of one day ducks?",
+            "Which player has scored the fastest century",
+            "Who is having the best one day bowling figures",
+            "Who was the first player to score a One Day International century",
+            "Who hit the winning runs in India’s famous NatWest final victory against England in 2002",
+            "Against which team did India win its first One Day International match",
+            "Who topped the batting averages for England in the 2003 World Cup",
+            "Who won 'Man of the Tournament' award in the 1992 World Cup",
+            "Who was Sri Lanka’s first one day captain",
+            "Against which team did Brian Lara’s hit the largest odi score",
+            "Which team did Sri Lanka dismiss for the lowest one day total ever",
+            "Name the umpire who has officiated in both the cricket and football World Cups",
+            "Who holds the record for most One Day International wickets",
+            "Who holds the record for most dismissals",
+            "Who holds the record for most nineties in career",
+            "Who holds the record for most catches in career (non wicket keeper)",
+            "Which was the 1st non Test playing country to beat India in an international match",
+            "Which team has played most number of Cricket Test Matches",
+            "Which team is holding the record of fewest runs in an inning in Test Cricket",
+            "Which Bowler has taken most number of 5 wickets in an innings in Test Cricket",
+            "Which player has Most matches played as captain in Test Cricket",
+            "NEITHER GODS NOR MEN WILL EVER COMPEL ME TO LET YOU TURN CASTERLY ROCK INTO YOUR WHOREHOUSE.",
+            "Who has the Highest Individual score in ODIs",
+            "Which was the 1st non Test playing country to beat India in an international match",
+            "Where was the final match of ICC World Cup 2007, between Australia and Sri Lanka, played", "India's highest run chase of 321 is against which team?",
+            "Which team is the highest chaser in ODI cricket",
+            "At Which Stadium did south africa made a record chase",
+            "Who bowled the first ball of icc world cup 2015",
+            "who was the first batsmen to get out in icc world cup 2015",
+            "Last bowl of the icc world cup was bowled by",
+            "world cup 1975 finals' highest scorer",
+            "who was the last man to get out in world cup 1975",
+            "Most no of matches as an Umpire",
+            "who is the slowest run scorer in test cricket",
+            "which player has the longest interval between appearances",
+            "Who has been the youngest captain in test cricket",
+            "Player with maximum no of times as test captain",
+            "To me, it doesn't matter how good you are. Sport is all about playing and competing. Whatever you do in cricket and in sport, enjoy it, be positive and try to win",
+            "A cricket ground is a flat piece of earth with some buildings around it",
+            "To me, cricket is a simple game. Keep it simple and just go out and play",
+            "You can cut the tension with a cricket stump",
+            "I have seen god. He bats at no.4 for india in tests",
+            "On the Off-side first there is God,then Sourav Ganguly",
+            "Cricket is a pressure game, and when it comes to India-Pakistan match the pressure is doubled",
+            "Reading poetry and watching cricket were the sum of my world, and the two are not so far apart as many aesthetes might believe",
+            "which player always puts on his left pad first while going out to bat",
+            "which player wears a jersey with the number 12 on it since his birthday is on the 12th of December, which also happens to be the 12th month of the year. He considers it to be his lucky number. He also wears a black thread on his wrist",
+            "which player listens to loud music before going out to bat",
+            "Born on July 7, he considers it as his lucky number. He sports number seven on his jersey",
+            "which indian seamer considers a yellow handkerchief as his lucky charm",
+            "which Australian captain used to carry a red handkerchief in his left pocket which was known to be given by his grandfather once"};
+
+    static String[] AnswerEasy = {"Sanath Jayasuriya", "Corey Anderson",
+            "Chaminda Vaas", "Dennis Amiss", "Zaheer Khan", "East Africa",
+            "Paul Collingwood", "Martin Crowe", "Anura Tennekoon", "Sri Lanka",
+            "Zimbabwe", "Steve Bucknor", "Muttiah Muralitharan", "Adam Gilchrist",
+            "Sachin Tendulkar", "Mahela Jayawardene", "Sri Lanka", "new zealand", "Muttiah Muralitharan",
+            "Graeme Smith", "rohit sharma", "Sri Lanka", "Barbados",
+            "Bridgetown", "Sri Lanka", "South Africa", "Wanderers Stadium",
+            "Nuwan Kulasekera", "BB McCullum", "Henry", "CH Lloyd",
+            "JR Thomson", "RE Koertzen", "Hanif Mohammad", "John Traicos",
+            "Tatenda Taibu", "Graeme Smith", "ian botham",
+            "richie benaud", "shane warne", "murray walker", "Matthew Hayden",
+            "Rahul dravid", "Imran khan", "Donald bradman", "sachin tendulkar",
+            "yuvraj Singh", "michael clarke", "Ms Dhoni", "zaheer khan", "Steve Waugh"};
+
+//	static String[] typeMed = { "t", "t", "t", "t", "t", "t", "t", "t", "t",
+//			"t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t",
+//			"t", "t", "t", "t", "t"};
+
+//	static String[] imagemed = {
+//			"https://lh5.googleusercontent.com/-kRwxpvmEGdw/VSexqbogdMI/AAAAAAAAAL0/EVBuQfp4cMI/w500-h400-no/Renly-Baratheon-house-baratheon-30540977-500-400.png",
+//			"https://lh6.googleusercontent.com/WcEalYN-ntgcVpPAQ5UEvqRO9FjYy23i0YW8b57J2yo=w455-h641-no",
+//			"https://lh3.googleusercontent.com/79Ya9P2XKj3mxxryR8d0_RRdlu5uF-eayWkcYW0tDI8=w516-h718-no",
+//			"https://lh6.googleusercontent.com/lBt1Px8tzWyGPGoxh6bS2NezecWA8LsR9wOiC44hQD4=w500-h703-no",
+//			"https://lh5.googleusercontent.com/yqbzO6HuHQcG7DXZLbC-MwPu3P-Xiaw8cbMNgDnm_40=w600-h345-no",
+//			"https://lh6.googleusercontent.com/z6DCGvkmA12iZBhfSXEgm5S58c80NQj-TTdhnHynZjY=w610-h343-no",
+//			"https://lh6.googleusercontent.com/T_6jBAGiFZktYP6F6V3y8RDfElm_ZpAIc0hzNUFVm20=w400-h322-no",
+//			"https://lh3.googleusercontent.com/pPP6ADj5Kd9gnU1YctTxFsCuhX-kVKBfXBWYgSNL82s=s500-no",
+//			"https://lh3.googleusercontent.com/8kGd2lLD537BTbELbX_5guXHPr_f_ehf88j-xYlu21c=w500-h744-no",
+//			"https://lh3.googleusercontent.com/9WcxnJ-ssNbny_8ET9bseNUpJzXiTJdWjMXrRLwHgpM=s450-no",
+//			"https://lh3.googleusercontent.com/WHPuWqTPzycPsXZyqALoKMDIdDQ-0X1MQLu2aUHNjAU=w600-h338-no",
+//			"https://lh5.googleusercontent.com/-esnFsT-6spI/VSe0w56ZPDI/AAAAAAAAAOs/XAwZ9BEngeg/w486-h265-no/MelisandreSlider.jpg",
+//			"https://lh4.googleusercontent.com/0yHCn7vIx0edlXZgm0-xIPEUkPOjCmXSOu959qMs5e4=s800-no",
+//			"https://lh4.googleusercontent.com/mxeyCoH5qpWhTtXPgL8uM69dGMPxaAepijKhhRiJ9cM=s512-no",
+//			"https://lh6.googleusercontent.com/hztk4Gohq-4bP95m-R4IovpN1hSwJU0pzedDZwtAEaM=w1366-h768-no",
+//			"https://lh4.googleusercontent.com/cOgbUtWPRHgWruT4VbO2_--3IHoLUy5qIvLaBuTO8AM=s512-no",
+//			"https://lh4.googleusercontent.com/NSrTRWH8sYL7_8FWC0JZkgylcjY31Lp-ze31GgmUMtg=s512-no",
+//			"", "", "", "", "", "", "", "", "", ""};
+
+//	static String[] textmed = {
+//
+//			};
+//
+//	static String[] ansmed = { };
+
+//	static String[] typehard = { "t", "t", "t", "t", "t", "t", "t", "t", "t",
+//			"t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t",
+//			"t" };
+//
+//	static String[] imagehard = {
+//			"https://lh4.googleusercontent.com/VCCAR7VyAvKAQxapOXHx6akPU7Hd_EpqF2_F39E6r54=w375-h547-no",
+//			"",
+//			"https://lh4.googleusercontent.com/-UbKUNUjMcmw/VSe3dohLIuI/AAAAAAAAAQY/4Qgkm2lBa70/w250-h375-no/Samwell%2BTarly.png",
+//			"https://lh4.googleusercontent.com/nJL1z2tZ3ugHYcw_tuxZuM_xskN0IW3lEWSGg7ZneZI=w720-h404-no",
+//			"https://lh4.googleusercontent.com/MoPLfqmPeDATWz6gwQc73GrdUcCL4uUJu0-5ax26PXc=s800-no",
+//			"https://lh4.googleusercontent.com/ap7iNUKAVuld-9zCQv6n7vga6yTn0uYKIM822C4XE2E=w512-h717-no",
+//			"",
+//			"https://lh3.googleusercontent.com/2RY7_zNWpKdZljCXdhRpB0OPPv_z_mIJpVXvV6ZLN94=w716-h673-no",
+//			"https://lh5.googleusercontent.com/9KRVcZphfe2ia2NOVEfHN4Gp5aqmASnnnunw53B_dxM=w500-h281-no",
+//			"",
+//			"https://lh3.googleusercontent.com/g5Rc7hNB5AuU9UhQ1DaCx_WLqyfMXqVPNDMHf5MXG6I=w500-h584-no",
+//			"https://lh5.googleusercontent.com/-dzth46kTcaE/VSe5LVpn2QI/AAAAAAAAASI/UjgTM2zGzI4/w640-h360-no/lysa_arryn.jpg",
+//			"https://lh3.googleusercontent.com/-H1UAmaruAOU/VSe5bFt0hAI/AAAAAAAAASY/1Dg9d1qLcIk/w535-h770-no/Roose-Bolton.jpg",
+//			"https://lh3.googleusercontent.com/-TbnrsflSUdE/VSe5y3oVWRI/AAAAAAAAASo/t4JHuOjb1mo/w403-h400-no/TalishaStark.jpg",
+//			"https://lh3.googleusercontent.com/Ljlmc8YhVoIuPaDKLecEjLAPxtmrtanBN0v43iOrEK0=w368-h555-no",
+//			"https://lh4.googleusercontent.com/-hmMeDmm9VIo/VSe6Tx--coI/AAAAAAAAATI/Wm0srOaTxHk/w500-h281-no/Osha.png",
+//			"https://lh3.googleusercontent.com/Y4y8l-I9rRZfRHSWF54c0O3E-iIIaCdhcWOqMeMMSms=w515-h287-no",
+//			"", "", "", "", "", "" };
+//
+//	static String[] texthard = {
+//			"",
+//			"The next time we see each other, we'll talk about your mother. I promise.",
+//			"",
+//			"",
+//			"",
+//			"",
+//			"A mind needs books as a sword needs a whetstone, if it is to keep its edge.",
+//			"",
+//			"",
+//			"War was easier than daughters.",
+//			"",
+//			"",
+//			"",
+//			"",
+//			"",
+//			"",
+//			"",
+//			"Money buys a man's silence for a time.A bolt in the heart buys it forever.",
+//			"The Mad King Did Aa He Liked. Has Your Uncle Jaime Ever Told You What Happened To Him?",
+//			"The next time you raise a hand to me will be the last time you have hands!",
+//			"Your Joy Will Turn to ashes in your mouth",
+//			"The Lannisters Send Their Regards",
+//			"Chaos Isn't A Pit. Chaos Is A Ladder" };
+//
+//	static String[] answerhard = { "Jorah Mormont", "Eddard Stark",
+//			"Samwell Tarly", "Ramsay Snow", "Baratheon", "Syrio Forel",
+//			"Tyrion Lannister", "Needle", "Viserys Targaryen", "Eddard Stark",
+//			"Shireen Baratheon", "Lysa Arryn", "Roose Bolton", "Talisa",
+//			"Hot Pie", "Osha", "Grey Worm", "Petyr Baelish",
+//			"Tyrion Lannister", "Daenerys", "Tyrion Lannister", "Roose Bolton",
+//			"Petyr Baelish" };
+//
+//	static String[] typevhard = { "t", "t", "t", "t", "t", "t", "t", "t", "t",
+//			"t", "t" };
+//
+//	static String[] imagevhard = {
+//			"https://lh4.googleusercontent.com/tNV08UxPwElkPovmHtEtPD_IfSc-wXxB4AnXbg-xM1c=w400-h300-no",
+//			"https://lh3.googleusercontent.com/-xUw3WypfJdY/VSe7QG5GuFI/AAAAAAAAAT4/JCcgzk9ur2g/w551-h828-no/Yara_Greyjoy.png",
+//			"https://lh3.googleusercontent.com/-bItg3w6NLMI/VSe7jsIVNNI/AAAAAAAAAUI/lsADoCwrYVQ/w320-h240-no/Pycelle.jpg",
+//			"https://lh6.googleusercontent.com/-xntO-mbb1a8/VSfBNS2EmSI/AAAAAAAAAWA/1qZtxRy8Fx0/w500-h281-no/Robert_Arry.jpg",
+//			"https://lh4.googleusercontent.com/-ER29tgn9E14/VSe9TEUbIkI/AAAAAAAAAUo/mpqAa5QzihE/w337-h481-no/Selyse%2BBaratheon.jpg",
+//			"https://lh6.googleusercontent.com/f6D7TquK2rPfa-o35EqFA0HLUuXWGvrfy6I7dnFcVCo=w486-h324-no",
+//			"",
+//			"https://lh3.googleusercontent.com/TCnVmfEudL7P_03G5n74PYnWKJd4lxG7miIGfjo42Lo=s829-no",
+//			"https://lh3.googleusercontent.com/B8-sawa8I8u7xiCzmVGkcOrqs6tp7A31NrvEI0XB6BA=w500-h281-no",
+//			"", "" };
+//
+//	static String[] textvhard = {
+//			"",
+//			"",
+//			"",
+//			"",
+//			"",
+//			"",
+//			"Storms come and go, the big fish eat the little fish, and I keep on paddling.",
+//			"",
+//			"",
+//			"What good is the word extravagant if it can't be used to describe a royal wedding?",
+//			"When Dead Men And Worse Come Hunting.You Think It Matters Who Sits On The Iron Throne?" };
+//
+//	static String[] answervhard = { "Aarakh", "Yara Greyjoy", "Pycelle",
+//			"Robert Arryn", "Selyse Baratheon", "Salladhor Saan", "Varys",
+//			"House Martell", "Rickon Stark", "Olenna Tyrell", "Jorah Mormont" };
+
+}
